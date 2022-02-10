04a205d16f579043eb21340ebb8bd07f62ecc867
F0urchette <mario.32@live.fr>
2022-02-10 23:49:20 +0100

    Cleaned a bit and push is working when exiting

--- git4school_watch_dog.py
+++ git4school_watch_dog.py
@@ -1,51 +1,19 @@
-import time
 import os
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
-from git import Repo
+import time
 
-def on_created(event):
-    print(f"{event.src_path} has been created!")
-
-
-def on_deleted(event):
-    print(f"Someone deleted {event.src_path}!")
-
-
-def on_modified(event):
-    print(f"{event.src_path} has been modified, git-committed !")
-    repo.index.add([event.src_path])
-    repo.index.commit(f"Git4school auto-commit: {os.path.basename(event.src_path)} has been modified")
-
-
-def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+from utils.Watcher import FileWatcherWatchdog
 
 if __name__ == "__main__":
-    repo = Repo(".")
-    patterns = ["*"]
-    ignore_patterns = ["*~"]
-    ignore_directories = True
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    folder_path = os.path.join('..', 'test-gitpython', 'src')
+    repo_path = os.path.join('..', 'test-gitpython')
+    ssh_path = os.path.join(os.path.expanduser('~'), '.ssh', 'id_rsa')
 
-    my_event_handler.on_created = on_created
-    my_event_handler.on_deleted = on_deleted
-    my_event_handler.on_modified = on_modified
-    my_event_handler.on_moved = on_moved
-
-    path = "./src"
-    go_recursively = True
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
-
-    my_observer.start()
+    file_watcher = FileWatcherWatchdog(folder_path, repo_path, ssh_path)
+    file_watcher.start()
+    
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         print("Fin")
-        my_observer.stop()
-        my_observer.join()
-
-
+        file_watcher.stop()
---
+++ utils/Watcher.py
@@ -0,0 +1,84 @@
+import os
+from abc import ABC, abstractmethod
+
+from git import Repo, RemoteProgress
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers.polling import PollingObserver as Observer
+
+
+class FileWatcherInterface(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class MyProgressPrinter(RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        print(op_code, cur_count, max_count, cur_count / (max_count or 100.0),
+              message or "NO MESSAGE")
+
+
+class FileWatcherWatchdog(FileWatcherInterface):
+
+    def start(self):
+        self.observer.start()
+
+    def stop(self):
+        self.push()
+        self.observer.stop()
+        self.observer.join()
+
+    def push(self):
+        try:
+            ssh_cmd = f'ssh -v -i {self.ssh_path}'
+            with self.repo.git.custom_environment(GIT_SSH_COMMAND=ssh_cmd):
+                self.origin.push()  # progress=MyProgressPrinter())
+                print("Pushed !")
+        except Exception as e:
+            print(e)
+
+    def __init__(self, folder_to_watch, repo_path, ssh_path):
+        self.repo = Repo(repo_path)
+        self.ssh_path = ssh_path
+        patterns = ["*"]
+        ignore_patterns = ["*~"]
+        ignore_directories = True
+        case_sensitive = True
+        my_event_handler = PatternMatchingEventHandler(patterns,
+                                                       ignore_patterns,
+                                                       ignore_directories,
+                                                       case_sensitive)
+        self.origin = self.repo.remote(name="origin")
+
+        print(self.origin.url)
+
+        my_event_handler.on_created = self.on_created
+        my_event_handler.on_deleted = self.on_deleted
+        my_event_handler.on_modified = self.on_modified
+        my_event_handler.on_moved = self.on_moved
+        self.observer = Observer()
+        self.observer.schedule(my_event_handler, folder_to_watch,
+                               recursive=True)
+
+    def on_created(self, event):
+        print(f"{event.src_path} has been created!")
+
+    def on_deleted(self, event):
+        print(f"Someone deleted {event.src_path}!")
+
+    def on_modified(self, event):
+        path = os.path.abspath(event.src_path)
+        print(
+            f"{path} has been modified, git-committed ! <-- {event}")
+        self.repo.index.add([path])
+        self.repo.index.commit(
+            f"Git4school auto-commit: "
+            f"{path} has been modified")
+
+    def on_moved(self, event):
+        print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
