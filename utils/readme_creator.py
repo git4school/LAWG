4192cf2313a4bb5e581d84d3c25f3729aab35e72
Mika <mario.32@live.fr>
2022-03-30 00:05:11 +0200

    ✨ Ask identity info at startup

--- utils/readme_creator.py
+++ utils/readme_creator.py
@@ -1,32 +1,30 @@
+import json
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 from prompt_toolkit.shortcuts import input_dialog, radiolist_dialog
 
 
-class ReadmeCreatorInterface(ABC):
+class IdentityCreatorInterface(ABC):
     def __init__(self):
         pass
 
-    def create_readme(self):
-        first_name = self.ask_first_name()
-        last_name = self.ask_last_name()
-        group = self.ask_group()
+    def create_identity_file(self, repo_path: Path, groups: list):
+        identity_file_path = Path(repo_path) / "IDENTITY.json"
 
-        readme = f"""
-# README
+        if not identity_file_path.exists():
+            first_name = self.ask_first_name()
+            last_name = self.ask_last_name()
+            group = self.ask_group(groups)
 
-### NOM : {last_name.upper()}
-### Prénom : {first_name.capitalize()}
-### Groupe de TP : {group}
-- [ ] 11
-- [x] 12
-- [ ] 21
-- [ ] 22
-"""
+            identity = {
+                "first_name": first_name,
+                "last_name": last_name,
+                "group": group
+            }
 
-        with open('READMEe.md', 'w') as readme_file:
-            readme_file.write(readme
-                              )
+            with open(identity_file_path, 'w') as identity_file:
+                json.dump(identity, identity_file, indent=4)
 
     @abstractmethod
     def ask_first_name(self) -> str:
@@ -37,34 +35,32 @@ class ReadmeCreatorInterface(ABC):
         pass
 
     @abstractmethod
-    def ask_group(self) -> str:
+    def ask_group(self, groups: list) -> str:
         pass
 
 
-class ReadmeCreatorDialog(ReadmeCreatorInterface):
+class IdentityCreatorDialog(IdentityCreatorInterface):
     def ask_first_name(self) -> str:
         first_name = input_dialog(
-            title='Initialisation du README',
-            text='Entrez votre prénom : ').run()
+            title='Creation of the identity file',
+            text='Please enter your first name: ').run()
 
         return first_name
 
     def ask_last_name(self) -> str:
         last_name = input_dialog(
-            title='Initialisation du README',
-            text='Entrez votre nom de famille : ').run()
+            title='Creation of the identity file',
+            text='Please enter your last name: ').run()
 
         return last_name
 
-    def ask_group(self) -> str:
+    def ask_group(self, groups: list) -> str:
+        groupss = [(groups[i], groups[i]) for i in range(len(groups))] if len(groups) > 0 else ["1",
+                                                                                                "2"]
         group = radiolist_dialog(
-            title="Initialisation du README",
-            text="Sélectionnez votre groupe de TP : ",
-            values=[
-                ("TP1", "TP1"),
-                ("TP2", "TP2"),
-                ("TP3", "TP3")
-            ]
+            title="Creation of the identity file",
+            text="Please select your group : ",
+            values=groupss
         ).run()
 
         return group
